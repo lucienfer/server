f713f7f636304d8fe18def0ff1d7be47b42ddce3
lucienfer <lucien.fernandez02@gmail.com>
2024-01-11 16:33:46 +0100

    modif index and connexion with braavos

--- build/src/index.js
+++ build/src/index.js
@@ -4,6 +4,11 @@ document.getElementById('connectButton').addEventListener('click', async () => {
 	try {
 		const wallet = await connect();
 		console.log('Wallet connecté:', wallet);
+		let img = new Image();
+		img.src = wallet.icon;
+		img.alt = 'Braavos';
+		document.getElementById('connectButton').textContent = wallet.selectedAddress;
+		document.getElementById('connectButton').appendChild(img);
 	} catch (error) {
 		console.error('Erreur de connexion:', error);
 	}
